f50ec7fc87cdf1668d1f82ec3d67466dfd8a1473
Reedych Coyote <reedych@reedych.pw>
2017-12-27 15:02:17 +0600

    Коммент

--- info.h
+++ info.h
@@ -7,6 +7,7 @@
 #define RSSVERSION "2.0"
 #define APIVERSION "5.67"
 #define APIKEY "e9f83d4ee9f83d4ee9f83d4eb5e9a50e2dee9f8e9f83d4eb09b0a7d8cf528f7fe3149cb"
+// возьмите свой ключ, этот не работает
 
 #define GROUP 1
 #define PAGE 0
